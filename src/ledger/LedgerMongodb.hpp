79cac46a21746622d766df4d7ad14848b52e706a
trax Omar Givernaud <omar.givernaud@neurochaintech.io>
2018-10-10 14:29:52 +0200

    reindent

--- src/ledger/LedgerMongodb.hpp
+++ src/ledger/LedgerMongodb.hpp
@@ -1,283 +1,259 @@
 #ifndef NEURO_SRC_LEDGERMONGODB_HPP
 #define NEURO_SRC_LEDGERMONGODB_HPP
 
+#include "common/logger.hpp"
+#include "config.pb.h"
 #include "ledger/Ledger.hpp"
 #include "ledger/mongo.hpp"
-#include "common/logger.hpp"
 #include "messages.pb.h"
-#include "config.pb.h"
 
-namespace neuro
-{
-namespace ledger
-{
+namespace neuro {
+namespace ledger {
 
-class LedgerMongodb : public Ledger
-{
-private:
-    mutable mongocxx::instance _instance{};
-    mutable mongocxx::uri _uri;
-    mutable mongocxx::client _client;
-    mutable mongocxx::database _db;
-    mutable mongocxx::collection _blocks;
-    mutable mongocxx::collection _transactions;
+class LedgerMongodb : public Ledger {
+ private:
+  mutable mongocxx::instance _instance{};
+  mutable mongocxx::uri _uri;
+  mutable mongocxx::client _client;
+  mutable mongocxx::database _db;
+  mutable mongocxx::collection _blocks;
+  mutable mongocxx::collection _transactions;
 
-    void remove_OID(mongocxx::options::find &filter) {}
+  void remove_OID(mongocxx::options::find &filter) {}
 
-    bool get_block_header(const bsoncxx::document::view &block,
-                          messages::BlockHeader *header)
-    {
-        messages::from_bson(block, header);
-        return true;
+  bool get_block_header(const bsoncxx::document::view &block,
+                        messages::BlockHeader *header) {
+    messages::from_bson(block, header);
+    return true;
+  }
+
+  // bool
+  // get_block(const bsoncxx::document::view &bson_block,
+  //           messages::Block *block);
+
+  bool get_transactions_from_block(const bsoncxx::document::view &id,
+                                   messages::Block *block) {
+    auto query = bss::document{} << "id" << id << bss::finalize;
+
+    mongocxx::options::find findoption;
+    auto projection_transaction = bss::document{};
+    projection_transaction << "_id" << 0;  ///!< remove _id objectID
+    findoption.projection(projection_transaction.view());
+
+    auto cursor = _transactions.find(std::move(query), findoption);
+
+    for (auto &bson_transaction : cursor) {
+      auto transaction = block->add_transactions();
+      from_bson(bson_transaction, transaction);
     }
 
-    // bool
-    // get_block(const bsoncxx::document::view &bson_block,
-    //           messages::Block *block);
+    return true;
+  }
 
-    bool get_transactions_from_block(const bsoncxx::document::view &id,
-                                     messages::Block *block)
-    {
-        auto query = bss::document{} << "id" << id << bss::finalize;
+  bool get_transactions_from_block(const messages::BlockID &id,
+                                   messages::Block *block) {
+    const auto bson_id = to_bson(id);
+    return get_transactions_from_block(bson_id.view(), block);
+  }
 
-        mongocxx::options::find findoption;
-        auto projection_transaction = bss::document{};
-        projection_transaction << "_id" << 0;  ///!< remove _id objectID
-        findoption.projection(projection_transaction.view());
+  void init_block0(messages::config::Database &db) {
+    messages::Block block0;
+    if (!get_block(0, &block0)) {
+      messages::Block block0file;
+      std::ifstream t("block.0.bp");
+      std::string str((std::istreambuf_iterator<char>(t)),
+                      std::istreambuf_iterator<char>());
+      auto d = bss::document{};
+      switch (db.block0_format()) {
+        case messages::config::Database::Block0Format::
+            Database_Block0Format_PROTO:
+          block0file.ParseFromString(str);
+          break;
+        case messages::config::Database::Block0Format::
+            Database_Block0Format_BSON:
+          d << str;
+          messages::from_bson(d.view(), &block0file);
+          break;
+        case messages::config::Database::Block0Format::
+            Database_Block0Format_JSON:
+          messages::from_json(str, &block0file);
+          break;
+      }
 
-        auto cursor = _transactions.find(std::move(query), findoption);
+      push_block(block0file);
+    }
+  }
 
-        for (auto &bson_transaction : cursor)
-        {
-            auto transaction = block->add_transactions();
-            from_bson(bson_transaction, transaction);
-        }
+ public:
+  LedgerMongodb(const std::string &url, const std::string &db_name);
+  LedgerMongodb(messages::config::Database &db);
 
-        return true;
+  ~LedgerMongodb() {}
+
+  messages::BlockHeight height() const {
+    auto query = bss::document{} << bss::finalize;
+    auto options = mongocxx::options::find{};
+    options.sort(bss::document{} << "height" << -1 << bss::finalize);
+
+    auto projection_transaction = bss::document{};
+    projection_transaction << "_id" << 0;  ///!< remove _id objectID
+    options.projection(projection_transaction.view());
+
+    const auto res = _blocks.find_one(std::move(query), options);
+    if (!res) {
+      return 0;
     }
 
-    bool get_transactions_from_block(const messages::BlockID &id,
-                                     messages::Block *block)
-    {
-        const auto bson_id = to_bson(id);
-        return get_transactions_from_block(bson_id.view(), block);
+    return res->view()["height"].get_int32().value;
+  }
+
+  bool get_block_header(const messages::BlockID &id,
+                        messages::BlockHeader *header) {
+    const auto bson_id = to_bson(id);
+
+    auto query = bss::document{} << "id" << bson_id << bss::finalize;
+
+    mongocxx::options::find findoption;
+    auto projection_transaction = bss::document{};
+    projection_transaction << "_id" << 0;  ///!< remove _id objectID
+    findoption.projection(projection_transaction.view());
+
+    auto res = _blocks.find_one(std::move(query), findoption);
+    if (!res) {
+      return false;
+    }
+    return get_block_header(res->view(), header);
+  }
+
+  bool get_last_block_header(messages::BlockHeader *block_header) {
+    auto query = bss::document{} << bss::finalize;
+    auto options = mongocxx::options::find{};
+    options.sort(bss::document{} << "height" << -1 << bss::finalize);
+
+    auto projection_transaction = bss::document{};
+    projection_transaction << "_id" << 0;  ///!< remove _id objectID
+    options.projection(projection_transaction.view());
+
+    const auto res = _blocks.find_one(std::move(query), options);
+    if (!res) {
+      return false;
     }
 
+    messages::from_bson(*res, block_header);
+    return true;
+  }
 
-    void init_block0( messages::config::Database &db )
-    {
-        messages::Block block0;
-        if (!get_block(0,&block0))
-        {
-            messages::Block block0file;
-            std::ifstream t("block.0.bp");
-            std::string str((std::istreambuf_iterator<char>(t)),
-                            std::istreambuf_iterator<char>());
-            auto d = bss::document{};
-            switch(db.block0_format() )
-            {
-            case messages::config::Database::Block0Format::Database_Block0Format_PROTO:
-                block0file.ParseFromString(str);
-                break;
-            case messages::config::Database::Block0Format::Database_Block0Format_BSON:
-                d << str;
-                messages::from_bson(d.view() , &block0file);
-                break;
-            case messages::config::Database::Block0Format::Database_Block0Format_JSON:
-                messages::from_json(str, &block0file);
-                break;
-            }
+  bool get_block(const messages::BlockID &id, messages::Block *block) {
+    auto header = block->mutable_header();
+    auto res_state = get_block_header(id, header);
 
+    res_state &= get_transactions_from_block(id, block);
 
+    return res_state;
+  }
 
-            push_block(block0file);
-        }
+  bool get_block(const messages::BlockHeight height, messages::Block *block) {
+    auto query = bss::document{} << "height" << height << bss::finalize;
+
+    mongocxx::options::find findoption;
+    auto projection_transaction = bss::document{};
+    projection_transaction << "_id" << 0;  ///!< remove _id objectID
+    findoption.projection(projection_transaction.view());
+
+    const auto res = _blocks.find_one(std::move(query), findoption);
+    if (!res) {
+      return false;
     }
 
-public:
-    LedgerMongodb(const std::string &url, const std::string &db_name);
-    LedgerMongodb(messages::config::Database &db);
+    std::cout << bsoncxx::to_json(res->view()) << std::endl;
 
-    ~LedgerMongodb() {}
+    auto header = block->mutable_header();
+    get_block_header(res->view(), header);
 
-    messages::BlockHeight height() const
-    {
-        auto query = bss::document{} << bss::finalize;
-        auto options = mongocxx::options::find{};
-        options.sort(bss::document{} << "height" << -1 << bss::finalize);
+    get_transactions_from_block(res->view()["id"].get_document(), block);
 
-        auto projection_transaction = bss::document{};
-        projection_transaction << "_id" << 0;  ///!< remove _id objectID
-        options.projection(projection_transaction.view());
+    return true;
+  }
 
-        const auto res = _blocks.find_one(std::move(query), options);
-        if (!res)
-        {
-            return 0;
-        }
+  bool push_block(const messages::Block &block) {
+    const auto header = block.header();
+    auto bson_header = messages::to_bson(header);
 
-        return res->view()["height"].get_int32().value;
+    _blocks.insert_one(std::move(bson_header));
+
+    for (neuro::messages::Transaction transaction : block.transactions()) {
+      if (!transaction.has_id()) {
+        transaction.mutable_id()->CopyFrom(header.id());
+      }
+
+      auto bson_transaction = messages::to_bson(transaction);
+      _transactions.insert_one(std::move(bson_transaction));
+    }
+    return true;
+  }
+
+  bool for_each(const Filter &filter, Functor functor) {
+    if (!filter.output()) {
+      return false;
     }
 
-    bool get_block_header(const messages::BlockID &id,
-                          messages::BlockHeader *header)
+    bss::document query_block;
+
+    ///!< TO DO
+    /*
+    if (filter.lower_height())
     {
-        const auto bson_id = to_bson(id);
-
-        auto query = bss::document{} << "id" << bson_id << bss::finalize;
-
-        mongocxx::options::find findoption;
-        auto projection_transaction = bss::document{};
-        projection_transaction << "_id" << 0;  ///!< remove _id objectID
-        findoption.projection(projection_transaction.view());
-
-        auto res = _blocks.find_one(std::move(query), findoption);
-        if (!res)
-        {
-            return false;
-        }
-        return get_block_header(res->view(), header);
+        query_block << "height"
+                    << bss::open_document
+                    << "$gte" << *filter.lower_height()
+                    << bss::close_document;
+    }
+    if (filter.upper_height())
+    {
+        query_block << "height"
+                    << bss::open_document
+                    << "$lte" << *filter.upper_height()
+                    << bss::close_document;
     }
 
-    bool get_last_block_header(messages::BlockHeader *block_header)
+    if (filter.block_id())
     {
-        auto query = bss::document{} << bss::finalize;
-        auto options = mongocxx::options::find{};
-        options.sort(bss::document{} << "height" << -1 << bss::finalize);
+        const auto bson = messages::to_bson(*filter.block_id());
+        query_block << "id" << bson;
+    }
+    */
 
-        auto projection_transaction = bss::document{};
-        projection_transaction << "_id" << 0;  ///!< remove _id objectID
-        options.projection(projection_transaction.view());
+    query_block << bss::finalize;
 
-        const auto res = _blocks.find_one(std::move(query), options);
-        if (!res)
-        {
-            return false;
-        }
+    auto query_transaction = bss::document{};
 
-        messages::from_bson(*res, block_header);
-        return true;
+    if (filter.output()) {
+      const auto bson = messages::to_bson(*filter.output());
+      query_transaction << "outputs.address" << bson;
+      std::cout << "adding output filter to query transaction" << std::endl;
     }
 
-    bool get_block(const messages::BlockID &id, messages::Block *block)
-    {
-        auto header = block->mutable_header();
-        auto res_state = get_block_header(id, header);
+    query_transaction << bss::finalize;
+    std::cout << "query_transaction " << bsoncxx::to_json(query_transaction) << std::endl;
 
-        res_state &= get_transactions_from_block(id, block);
+    // auto cursor_block = _blocks.find(query_block.view());
 
-        return res_state;
+    mongocxx::options::find findoption;
+    auto projection_transaction = bss::document{};
+    projection_transaction << "_id" << 0;  ///!< remove _id objectID
+    findoption.projection(projection_transaction.view());
+
+    auto cursor_transaction =
+      _transactions.find(query_transaction.view(), findoption);
+
+    for (auto &bson_transaction : cursor_transaction) {
+      messages::Transaction transaction;
+      messages::from_bson(bson_transaction, &transaction);
+      functor(transaction);
     }
 
-    bool get_block(const messages::BlockHeight height, messages::Block *block)
-    {
-        auto query = bss::document{} << "height" << height << bss::finalize;
-
-        mongocxx::options::find findoption;
-        auto projection_transaction = bss::document{};
-        projection_transaction << "_id" << 0;  ///!< remove _id objectID
-        findoption.projection(projection_transaction.view());
-
-        const auto res = _blocks.find_one(std::move(query), findoption);
-        if (!res)
-        {
-            return false;
-        }
-
-        std::cout << bsoncxx::to_json(res->view()) << std::endl;
-
-        auto header = block->mutable_header();
-        get_block_header(res->view(), header);
-
-        get_transactions_from_block(res->view()["id"].get_document(), block);
-
-        return true;
-    }
-
-    bool push_block(const messages::Block &block)
-    {
-        const auto header = block.header();
-        auto bson_header = messages::to_bson(header);
-
-        _blocks.insert_one(std::move(bson_header));
-
-        for (neuro::messages::Transaction transaction : block.transactions())
-        {
-            if (!transaction.has_id())
-            {
-                transaction.mutable_id()->CopyFrom(header.id());
-            }
-
-            auto bson_transaction = messages::to_bson(transaction);
-            _transactions.insert_one(std::move(bson_transaction));
-        }
-        return true;
-    }
-
-    bool for_each(const Filter &filter, Functor functor)
-    {
-        if (!filter.output())
-        {
-            return false;
-        }
-
-        bss::document query_block;
-
-        ///!< TO DO
-        /*
-        if (filter.lower_height())
-        {
-            query_block << "height"
-                        << bss::open_document
-                        << "$gte" << *filter.lower_height()
-                        << bss::close_document;
-        }
-        if (filter.upper_height())
-        {
-            query_block << "height"
-                        << bss::open_document
-                        << "$lte" << *filter.upper_height()
-                        << bss::close_document;
-        }
-
-        if (filter.block_id())
-        {
-            const auto bson = messages::to_bson(*filter.block_id());
-            query_block << "id" << bson;
-        }
-        */
-
-        query_block << bss::finalize;
-
-        auto query_transaction = bss::document{};
-
-        if (filter.output())
-        {
-            const auto bson = messages::to_bson(*filter.output());
-            query_transaction << "outputs.address" << bson;
-        }
-
-        query_transaction << bss::finalize;
-
-        // auto cursor_block = _blocks.find(query_block.view());
-
-        mongocxx::options::find findoption;
-        auto projection_transaction = bss::document{};
-        projection_transaction << "_id" << 0;  ///!< remove _id objectID
-        findoption.projection(projection_transaction.view());
-
-        auto cursor_transaction =
-            _transactions.find(query_transaction.view(), findoption);
-
-        for (auto &bson_transaction : cursor_transaction)
-        {
-            messages::Transaction transaction;
-            messages::from_bson(bson_transaction, &transaction);
-            functor(transaction);
-        }
-
-        return true;
-    }
+    return true;
+  }
 };
 
 }  // namespace ledger
--- src/tooling/blockg.cpp
+++ src/tooling/blockg.cpp
@@ -8,13 +8,12 @@
 #include "messages/Hasher.hpp"
 #include "messages/Message.hpp"
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 namespace po = boost::program_options;
 
-namespace neuro
-{
+namespace neuro {
 
 inline bool exists_file(const std::string &name) {
   struct stat buffer;
@@ -22,167 +21,147 @@ inline bool exists_file(const std::string &name) {
 }
 
 void coinbase(const crypto::EccPub &key_pub, const messages::NCCSDF &ncc,
-              messages::Transaction &transaction)
-{
-    // messages::Transaction transaction;
-    Buffer key_pub_raw;
-    key_pub.save(&key_pub_raw);
-    messages::Hasher address(key_pub_raw);
+              messages::Transaction &transaction) {
+  // messages::Transaction transaction;
+  Buffer key_pub_raw;
+  key_pub.save(&key_pub_raw);
+  messages::Hasher address(key_pub_raw);
 
-    auto input = transaction.add_inputs();
+  auto input = transaction.add_inputs();
 
-    auto input_id = input->mutable_id();
-    input_id->set_type(messages::Hash::SHA256);
-    input_id->set_data("");
-    input->set_output_id(0);
-    input->set_key_id(0);
+  auto input_id = input->mutable_id();
+  input_id->set_type(messages::Hash::SHA256);
+  input_id->set_data("");
+  input->set_output_id(0);
+  input->set_key_id(0);
 
-    auto output = transaction.add_outputs();
-    output->mutable_address()->CopyFrom(address);
-    output->mutable_value()->CopyFrom(ncc);
-    transaction.mutable_fees()->set_value("0");
-    // return transaction;
+  auto output = transaction.add_outputs();
+  output->mutable_address()->CopyFrom(address);
+  output->mutable_value()->CopyFrom(ncc);
+  transaction.mutable_fees()->set_value("0");
+  // return transaction;
 }
 
-void block0( uint32_t bots,
-             const std::string &pathdir,
-             messages::NCCSDF &nccsdf,
-             ledger::LedgerMongodb &ledger
-           )
-{
-    LOG_INFO << "In Block 0";
-    messages::Block b;
-    messages::BlockHeader *header = b.mutable_header();
+void block0(uint32_t bots, const std::string &pathdir, messages::NCCSDF &nccsdf,
+            ledger::LedgerMongodb &ledger) {
+  LOG_INFO << "In Block 0";
+  messages::Block b;
+  messages::BlockHeader *header = b.mutable_header();
+  
+  LOG_INFO << "Ecc owner block0";
+  crypto::Ecc ecc;
+  Buffer key_pub_raw;
+  ecc.mutable_public_key()->save(&key_pub_raw);
 
-    LOG_INFO << "Ecc owner block0";
+  LOG_INFO << "Header block0";
+  messages::KeyPub *author = header->mutable_author();
+  author->set_type(messages::KeyType::ECP256K1);
+  author->set_raw_data(key_pub_raw.data(), key_pub_raw.size());
+
+  header->mutable_timestamp()->set_data(time(0));
+
+  LOG_INFO << "Prev block0";
+  auto previons_block_hash = header->mutable_previous_block_hash();
+  previons_block_hash->set_data("");  ///!< load 0
+  previons_block_hash->set_type(messages::Hash::Type::Hash_Type_SHA256);
+
+  header->set_height(0);
+
+  LOG_INFO << "Bot(s) block0";
+  ///!< bots generateur
+  for (uint32_t i = 0; i < bots; ++i) {
+    // gen i keys
     crypto::Ecc ecc;
-    Buffer key_pub_raw;
-    ecc.mutable_public_key()->save(&key_pub_raw);
+    ecc.save({pathdir + "/key_" + std::to_string(i) + ".priv"},
+             {pathdir + "/key_" + std::to_string(i) + ".pub"});
+    messages::Transaction *transaction = b.add_transactions();
+    coinbase(ecc.public_key(), nccsdf, *transaction);
+  }
 
-    LOG_INFO << "Header block0";
-    messages::KeyPub *author = header->mutable_author();
-    author->set_type(messages::KeyType::ECP256K1);
-    author->set_raw_data(key_pub_raw.data(), key_pub_raw.size());
+  // const auto tmp = crypto::hash_sha3_256( b.SerializeAsString() );
+  neuro::Buffer tmpbuffer(b.SerializeAsString());
+  messages::Hasher hash_id(tmpbuffer);
+  auto id = header->mutable_id();
+  id->CopyFrom(hash_id);
+  id->set_type(messages::Hash::SHA256);
 
-    header->mutable_timestamp()->set_data(time(0));
+  ledger.push_block(b);
 
-    LOG_INFO << "Prev block0";
-    auto previons_block_hash = header->mutable_previous_block_hash();
-    previons_block_hash->set_data("");  ///!< load 0
-    previons_block_hash->set_type(messages::Hash::Type::Hash_Type_SHA256);
-
-    header->set_height(0);
-
-    LOG_INFO << "Bot(s) block0";
-    ///!< bots generateur
-    for(uint32_t i = 0; i < bots ; ++i)
-    {
-        // gen i keys
-        crypto::Ecc ecc;
-        ecc.save({pathdir + "/key_" + std::to_string(i) + ".priv" } ,{pathdir + "/key_" + std::to_string(i) + ".pub"});
-        messages::Transaction *transaction = b.add_transactions();
-        coinbase(ecc.public_key(), nccsdf, *transaction);
-    }
-
-
-    header->mutable_id()->set_data("");
-
-    // const auto tmp = crypto::hash_sha3_256( b.SerializeAsString() );
-    neuro::Buffer tmpbuffer(b.SerializeAsString());
-    messages::Hasher hash_id(tmpbuffer);
-    header->mutable_id()->CopyFrom(hash_id);
-
-    ledger.push_block(b);
-
-
-
-    std::ofstream blockfile0;
-    blockfile0.open("block.0.bp");
-    blockfile0 << b.SerializeAsString();
-    blockfile0.close();
-
+  std::ofstream blockfile0;
+  blockfile0.open("block.0.bp");
+  blockfile0 << b.SerializeAsString();
+  blockfile0.close();
 }
 
-int main(int argc, char *argv[])
-{
-    po::options_description desc("Allowed options");
-    desc.add_options()("help,h", "Produce help message.")
-    ("wallet,w" , po::value<uint32_t>()->default_value(100),
-     "Nombre of Wallet in BLock 0")
-    ("keyspath,k", po::value<std::string>()->default_value("keys"),
-     "File path for keys (appending .pub or .priv)")
-    ("ncc,n", po::value<uint64_t>()->default_value(1000),
-     "How many ncc you want")
-    ("configuration,c", po::value<std::string>()->default_value("bot.json"),
-     "Configuration path.")
-    ;
+int main(int argc, char *argv[]) {
+  po::options_description desc("Allowed options");
+  desc.add_options()("help,h", "Produce help message.")(
+      "wallet,w", po::value<uint32_t>()->default_value(100),
+      "Nombre of Wallet in BLock 0")(
+      "keyspath,k", po::value<std::string>()->default_value("keys"),
+      "File path for keys (appending .pub or .priv)")(
+      "ncc,n", po::value<uint64_t>()->default_value(1000),
+      "How many ncc you want")(
+      "configuration,c", po::value<std::string>()->default_value("bot.json"),
+      "Configuration path.");
 
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    try
-    {
-        po::notify(vm);
-    }
-    catch (po::error &e)
-    {
-        return 1;
-    }
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  try {
+    po::notify(vm);
+  } catch (po::error &e) {
+    return 1;
+  }
 
-    if (vm.count("help"))
-    {
-        std::cout << desc << "\n";
-        return 1;
-    }
+  if (vm.count("help")) {
+    std::cout << desc << "\n";
+    return 1;
+  }
 
-    ///!< Load Config
-    messages::config::Config _config;
-    const auto configuration_filepath = vm["configuration"].as<std::string>();
-    messages::from_json_file(configuration_filepath, &_config);
+  ///!< Load Config
+  messages::config::Config _config;
+  const auto configuration_filepath = vm["configuration"].as<std::string>();
+  messages::from_json_file(configuration_filepath, &_config);
 
-    std::string jsonstr;
-    messages::to_json(_config,&jsonstr);
+  std::string jsonstr;
+  messages::to_json(_config, &jsonstr);
 
-    LOG_INFO << jsonstr;
+  LOG_INFO << jsonstr;
 
-    ///!< Log
-    if (_config.has_logs())
-    {
-        log::from_config(_config.logs());
-    }
+  ///!< Log
+  if (_config.has_logs()) {
+    log::from_config(_config.logs());
+  }
 
-    LOG_INFO << "Load DB ...";
+  LOG_INFO << "Load DB ...";
+  /*
+  if (!_config.has_database()) {
+    // ledger_ = std::make_shared<ledger::LedgerMongodb>(_config.db().url(),
+    LOG_INFO << "Database require";
+    return 1;
+  }
 
-    if (!_config.has_database()){
-       //ledger_ = std::make_shared<ledger::LedgerMongodb>(_config.db().url(),
-       LOG_INFO << "Database require";
-       return 1;
-    }
+  return 0;*/
 
-    return 0;
+  std::string keypath = vm["keyspath"].as<std::string>();
+  if (!exists_file(keypath)) {
+    std::cout << "create dir : " << std::endl;
+    std::cout << "mkdir " << keypath << std::endl;
+    return 1;
+  }
 
-    std::string keypath = vm["keyspath"].as<std::string>();
-    if ( !exists_file(keypath))
-    {
-        std::cout << "create dir : " << std::endl;
-        std::cout << "mkdir " << keypath << std::endl;
-        return 1;
-    }
+  uint32_t bots = vm["wallet"].as<uint32_t>();
+  uint64_t ncc = vm["ncc"].as<uint64_t>();
 
-    uint32_t bots = vm["wallet"].as<uint32_t>();
-    uint64_t ncc = vm["ncc"].as<uint64_t>();
+  messages::NCCSDF nccsdf;
+  nccsdf.set_value(std::to_string(ncc));
+  auto db = _config.database();
+  ledger::LedgerMongodb ledger("mongodb://127.0.0.1:27017/neuro","neuro");
+  block0(bots, keypath, nccsdf, ledger);
 
-    messages::NCCSDF nccsdf;
-    nccsdf.set_value(std::to_string(ncc));
-    auto db = _config.database();
-    ledger::LedgerMongodb ledger(db);
-    block0(bots,keypath ,  nccsdf, ledger);
-
-    return 0;
+  return 0;
 }
 
 }  // namespace neuro
 
-int main(int argc, char *argv[])
-{
-    return neuro::main(argc, argv);
-}
+int main(int argc, char *argv[]) { return neuro::main(argc, argv); }
