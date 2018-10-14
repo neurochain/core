f444770d2d597d514d796fa77155263358e21906
Riad Neuro <riad.saboundji@neurochaintech.io>
2018-10-14 17:47:17 +0200

    add transaction pool

--- src/consensus/PiiConsensus.hpp
+++ src/consensus/PiiConsensus.hpp
@@ -7,6 +7,8 @@
 #include "ForkManager.hpp"
 #include "Pii.hpp"
 #include "common/types.hpp"
+#include "crypto/Ecc.hpp"
+#include "TransactionPool.hpp"
 
 namespace neuro {
 namespace ledger {
@@ -23,6 +25,9 @@ namespace consensus {
 class PiiConsensus : public Pii, public Consensus {
  private:
   std::shared_ptr<ledger::Ledger> _ledger;
+  std::map<const Address , const crypto::Ecc *> _wallets_keys;
+  TransactionPool _transaction_pool;
+
   ForkManager _ForkManager;
   bool _valide_block;
   uint32_t _last_heigth_block;
@@ -36,14 +41,17 @@ class PiiConsensus : public Pii, public Consensus {
  public:
   PiiConsensus(std::shared_ptr<ledger::Ledger> ledger)
       : PiiConsensus(ledger, 2000) {}
-  PiiConsensus(std::shared_ptr<ledger::Ledger> ledger, uint32_t block_assembly);
 
+  PiiConsensus(std::shared_ptr<ledger::Ledger> ledger, uint32_t block_assembly);
+  void add_transaction(const messages::Transaction &transaction);
   void add_block(const neuro::messages::Block &block);
   void add_blocks(const std::vector<neuro::messages::Block *> &blocks);
   Address get_next_owner() const;
 
   bool check_owner(const neuro::messages::BlockHeader &blockheader) const;
   void show_owner(uint32_t start, uint32_t how = 10);
+
+  void add_wallet_keys(const crypto::Ecc *wallet);
 };
 
 }  // namespace consensus
