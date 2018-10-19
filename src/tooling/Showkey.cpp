1780b563f09427e8ae8b0d557d638dc7247fd5e8
Riad Neuro <riad.saboundji@neurochaintech.io>
2018-10-19 17:22:02 +0200

    add showkey

---
+++ src/tooling/Showkey.cpp
@@ -0,0 +1,45 @@
+#include <boost/program_options.hpp>
+#include "crypto/Ecc.hpp"
+#include "messages.pb.h"
+#include "messages/Hasher.hpp"
+
+namespace po = boost::program_options;
+
+namespace neuro {
+
+int main(int argc, char *argv[]) {
+  po::options_description desc("Allowed options");
+  desc.add_options()("help,h", "Produce help message.")(
+      "private,k", po::value<std::string>()->default_value("key.priv"),
+      "File path for private keys (appending .priv)")(
+      "public,p", po::value<std::string>()->default_value("key.priv"),
+      "File path for private keys (appending .pub)");
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  try {
+    po::notify(vm);
+  } catch (po::error &e) {
+    return 1;
+  }
+
+  if (vm.count("help")) {
+    std::cout << desc << "\n";
+    return 1;
+  }
+
+  crypto::Ecc ecc(vm["private"].as<std::string>(),
+                  vm["public"].as<std::string>());
+  std::cout << "public:  " << std::endl << ecc.public_key() << std::endl;
+  std::cout << "private: " << std::endl << ecc.private_key() << std::endl;
+  Buffer buf;
+  ecc.public_key().save(&buf);
+  messages::Hasher addr(buf);
+  std::cout << "address : " << std::endl << addr << std::endl;
+
+  return 0;
+}
+
+}  // namespace neuro
+
+int main(int argc, char *argv[]) { return neuro::main(argc, argv); }
