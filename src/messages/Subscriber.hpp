33f39508b59a6d0a4099d541b757c1159c5d4dce
trax Omar Givernaud <omar.givernaud@neurochaintech.io>
2018-10-24 11:20:28 +0200

    dedups message in subscriber
    this should be moved later to the queue to be done only once

--- src/messages/Subscriber.hpp
+++ src/messages/Subscriber.hpp
@@ -4,6 +4,7 @@
 #include <set>
 #include <typeindex>
 #include <typeinfo>
+#include "crypto/Hash.hpp"
 #include "messages.pb.h"
 #include "messages/Queue.hpp"
 
@@ -18,6 +19,8 @@ class Subscriber {
   mutable std::mutex _mutex_handler;
   std::shared_ptr<Queue> _queue;
   std::vector<std::vector<Callback>> _callbacks_by_type;
+  std::unordered_set<std::shared_ptr<Buffer>> _seen_messages_hash;
+  std::map<std::time_t, std::shared_ptr<Buffer>> _message_hash_by_ts;
 
  public:
   Subscriber(std::shared_ptr<Queue> queue)
@@ -31,12 +34,39 @@ class Subscriber {
 
   void unsubscribe() { _queue->unsubscribe(this); }
 
+  bool is_new_body(const std::time_t current_time, const messages::Body &body) {
+    // TOOD move this to the queue
+    Buffer serialized_body;
+    to_buffer(body, &serialized_body);
+
+    const auto hash =
+        std::make_shared<Buffer>(crypto::hash_sha3_256(serialized_body));
+    const auto &[it, is_emplaced] = _seen_messages_hash.emplace(hash);
+    if (!is_emplaced) {
+      LOG_TRACE << "message already seened " << body;
+      return false;
+    }
+
+    _message_hash_by_ts.emplace(std::piecewise_construct,
+                                std::forward_as_tuple(current_time),
+                                std::forward_as_tuple(hash));
+    LOG_TRACE << "new message " << body;
+    _message_hash_by_ts.erase(
+        _message_hash_by_ts.begin(),
+        _message_hash_by_ts.lower_bound(current_time - MESSAGE_TTL));
+
+    return true;
+  }
+
   void handler(std::shared_ptr<const Message> message) {
     std::lock_guard<std::mutex> lock_handler(_mutex_handler);
+    const auto time = std::time(nullptr);
     for (const auto &body : message->bodies()) {
       const auto type = get_type(body);
       for (const auto &cb : _callbacks_by_type[type]) {
-        cb(message->header(), body);
+        if (is_new_body(time, body)) {
+          cb(message->header(), body);
+        }
       }
     }
   }
