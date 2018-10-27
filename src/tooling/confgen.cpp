579700f4269e0f7f1ab760c16d84401ce4de2257
trax Omar Givernaud <omar.givernaud@neurochaintech.io>
2018-10-27 11:18:12 +0200

    missing confgen

---
+++ src/tooling/confgen.cpp
@@ -0,0 +1,84 @@
+#include <boost/program_options.hpp>
+#include <fstream>
+#include <vector>
+
+#include "config.pb.h"
+#include "crypto/Ecc.hpp"
+#include "messages.pb.h"
+#include "messages/Message.hpp"
+
+namespace neuro {
+namespace po = boost::program_options;
+
+int main(int argc, char *argv[]) {
+  po::options_description desc("Allowed options");
+  desc.add_options()("help,h", "Produce help message.")(
+      "keys,k", po::value<std::vector<std::string>>()->multitoken(),
+      "Key file to include")(
+      "endpoint,e", po::value<std::vector<std::string>>()->multitoken(),
+      "Endpoint in same order as keys")("conf,c",
+                                        po::value<std::string>()->required(),
+                                        "Configuration file to update")(
+      "output,o", po::value<std::string>()->required(), "Output filepath");
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  try {
+    po::notify(vm);
+  } catch (po::error &e) {
+    std::cout << e.what() << std::endl;
+    std::cout << desc << "\n";
+    return 1;
+  }
+  if (vm.count("help")) {
+    std::cout << "usage: " << desc << "\n";
+    return 1;
+  }
+
+  const auto conf_path = vm["conf"].as<std::string>();
+  const auto keys_path = vm["keys"].as<std::vector<std::string>>();
+  const auto endpoints = vm["endpoint"].as<std::vector<std::string>>();
+  const auto output = vm["output"].as<std::string>();
+
+  messages::config::Config config;
+  if (!messages::from_json_file(conf_path, &config)) {
+    std::cout << "Could not load configuration" << std::endl;
+    std::cout << desc << "\n";
+    return 1;
+  }
+
+  if (keys_path.size() != endpoints.size()) {
+    std::cout << "keys and endpoints should have the same number of elements";
+    std::cout << desc << "\n";
+    return 1;
+  }
+
+  auto itKey = keys_path.cbegin();
+  auto endKey = keys_path.cend();
+  auto itEndpoint = endpoints.cbegin();
+
+  while (itKey != endKey) {
+    messages::KeyPub key_pub;
+    crypto::EccPub ecc_pub(*itKey);
+    ecc_pub.save(&key_pub);
+    auto peer = config.mutable_networking()->mutable_tcp()->add_peers();
+    peer->set_endpoint(*itEndpoint);
+    peer->set_port(1337);
+    peer->mutable_key_pub()->CopyFrom(key_pub);
+
+    ++itKey;
+    ++itEndpoint;
+  }
+  std::string s;
+  messages::to_json(config, &s);
+
+  std::ofstream out(output);
+  out << s;
+  out.close();
+
+  return 0;
+}
+
+}  // namespace neuro
+
+int main(int argc, char *argv[]) { return neuro::main(argc, argv); }
