13fc39739afbc1dd2aa39ba3c7d5c3e7c0016ec8
Hermann <hermann.bouly@neurchaintech.io>
2018-10-29 14:02:13 +0100

    Backport Networking tests Ref #15

---
+++ tests/networking/TransportLayer.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+namespace neuro {
+namespace test {
+
+TEST(TransportLayer, ID_Uniqueness) {
+  // todo
+}
+
+} // namespace test
+} // namespace neuro
---
+++ tests/networking/tcp/Tcp.cpp
@@ -0,0 +1,40 @@
+#include "networking/tcp/Tcp.hpp"
+#include "crypto/Ecc.hpp"
+#include "messages/Message.hpp"
+#include "networking/Networking.hpp"
+#include "networking/TransportLayer.hpp"
+#include <gtest/gtest.h>
+
+namespace neuro {
+
+namespace test {
+
+class Tcp {
+public:
+  Tcp() = default;
+
+  bool test_connection() {
+    auto queue = std::make_shared<messages::Queue>();
+    auto keys1 = std::make_shared<crypto::Ecc>(),
+         keys2 = std::make_shared<crypto::Ecc>();
+    networking::Tcp tcp1(queue, keys1);
+    networking::Tcp tcp2(queue, keys2);
+    Port port{31212}; // Maybe change this to the port to be used by the bot
+    tcp1.accept(port);
+    auto peer = std::make_shared<messages::Peer>();
+    peer->set_endpoint("127.0.0.1");
+    peer->set_port(port);
+    tcp2.connect(peer);
+    tcp1._io_service.run_one();
+    tcp2._io_service.run_one();
+    return tcp1._connections.size() == 1 && tcp2._connections.size() == 1;
+  }
+};
+
+TEST(Tcp, ConnectionTest) {
+  Tcp tcp_test;
+  ASSERT_TRUE(tcp_test.test_connection());
+}
+
+} // namespace test
+} // namespace neuro
