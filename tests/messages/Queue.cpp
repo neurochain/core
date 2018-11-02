67946557f259a6b9b24fda4198e09283c86e815b
Hermann <hermann.bouly@neurchaintech.io>
2018-11-02 14:14:05 +0100

    Set right format Ref #15

--- tests/messages/Queue.cpp
+++ tests/messages/Queue.cpp
@@ -6,10 +6,8 @@
 #include "src/messages/Queue.hpp"
 #include "src/messages/Subscriber.hpp"
 
-namespace neuro
-{
-namespace test
-{
+namespace neuro {
+namespace test {
 
 template <typename T, typename... U>
 size_t function_address(std::function<T(U...)> f)
@@ -28,8 +26,7 @@ void callback_hello2(const messages::Header &header,
 void callback_world1(const messages::Header &header,
                      const messages::Body &body) {}
 
-class QueueTest
-{
+class QueueTest {
 public:
   bool test_empty() {
     auto tested_queue = std::make_shared<messages::Queue>();
@@ -39,8 +36,7 @@ public:
     return true;
   }
 
-  bool test_subscribe()
-  {
+  bool test_subscribe() {
     auto tested_queue = std::make_shared<messages::Queue>();
     messages::Subscriber sub_0(tested_queue), sub_1(tested_queue), sub_2(tested_queue);
     sub_0.subscribe(messages::Type::kHello, callback_hello1);
@@ -53,8 +49,7 @@ public:
       return false;
     }
     std::size_t count_sub_0(0), count_sub_1(0), count_sub_2(0);
-    for (const auto &sub : tested_queue->_subscribers)
-    {
+    for (const auto &sub : tested_queue->_subscribers) {
       if (sub == &sub_0) {
         ++count_sub_0;
       } else if (sub == &sub_1) {
@@ -65,15 +60,13 @@ public:
         return false;
       }
     }
-    if (count_sub_0 != 1 || count_sub_1 != 1 || count_sub_2 != 1)
-    {
+    if (count_sub_0 != 1 || count_sub_1 != 1 || count_sub_2 != 1) {
       return false;
     }
     return true;
   }
 
-  bool test_pushing_message()
-  {
+  bool test_pushing_message() {
     auto tested_queue = std::make_shared<messages::Queue>();
     tested_queue->publish(std::make_shared<const messages::Message>());
     if (tested_queue->_queue.empty()) {
@@ -82,22 +75,21 @@ public:
     return true;
   };
 
-  bool test_message_broadcasting()
-  {
+  bool test_message_broadcasting() {
     std::size_t count_hello(0);
     std::size_t count_world(0);
     auto tested_queue = std::make_shared<messages::Queue>();
     tested_queue->run();
     messages::Subscriber sub_hello(tested_queue);
     sub_hello.subscribe(messages::Type::kHello,
-     [&count_hello](const messages::Header &, const messages::Body &) { ++count_hello; });
+                        [&count_hello](const messages::Header &, const messages::Body &) { ++count_hello; });
     messages::Subscriber sub_world(tested_queue);
     sub_world.subscribe(messages::Type::kWorld,
-     [&count_world](const messages::Header &, const messages::Body &) { ++count_world; });
+                        [&count_world](const messages::Header &, const messages::Body &) { ++count_world; });
     tested_queue->subscribe(&sub_hello);
     tested_queue->subscribe(&sub_world);
     auto message_hello = std::make_shared<messages::Message>();
-    messages::Hello* hello = message_hello->add_bodies()->mutable_hello();
+    messages::Hello *hello = message_hello->add_bodies()->mutable_hello();
     if (hello == nullptr) {
       return false;
     }
@@ -108,7 +100,7 @@ public:
     hello_kpub->set_type(messages::ECP256K1);
     hello_kpub->set_raw_data("MIIBMzCB7AYHKoZIzj0CATCB4AIBATAsBgcqhkjOPQEBAiEA/////////////////////////////////////v///C8wRAQgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHBEEEeb5mfvncu6xVoGKVzocLBwKb/NstzijZWfKBWxb4F5hIOtp3JqPEZV2k+/wOEQio/Re0SKaFVBmcR9CP+xDUuAIhAP////////////////////66rtzmr0igO7/SXozQNkFBAgEBA0IABOBPdJmNMRu7dZ0O4+b/jG5CyuLeI870VKYu0DrtJ8I8VW3wt5NcbqfqIk7OI0+9cE7+xCPtKwF1vAHi730nMJ0=");
     auto message_world = std::make_shared<messages::Message>();
-    messages::World* world = message_world->add_bodies()->mutable_world();
+    messages::World *world = message_world->add_bodies()->mutable_world();
     if (world == nullptr) {
       return false;
     }
@@ -119,8 +111,7 @@ public:
     world_kpub->set_type(messages::ECP256K1);
     world_kpub->set_raw_data("MIIBMzCB7AYHKoZIzj0CATCB4AIBATAsBgcqhkjOPQEBAiEA/////////////////////////////////////v///C8wRAQgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHBEEEeb5mfvncu6xVoGKVzocLBwKb/NstzijZWfKBWxb4F5hIOtp3JqPEZV2k+/wOEQio/Re0SKaFVBmcR9CP+xDUuAIhAP////////////////////66rtzmr0igO7/SXozQNkFBAgEBA0IABOBPdJmNMRu7dZ0O4+b/jG5CyuLeI870VKYu0DrtJ8I8VW3wt5NcbqfqIk7OI0+9cE7+xCPtKwF1vAHi730nMJ0=");
     world->set_accepted(true);
-    if (!tested_queue->publish(std::move(message_hello)))
-    {
+    if (!tested_queue->publish(std::move(message_hello))) {
       return false;
     }
     if (!tested_queue->publish(std::move(message_world))) {
@@ -131,26 +122,22 @@ public:
   }
 };
 
-TEST(Queue, empty)
-{
+TEST(Queue, empty) {
   QueueTest mqt;
   ASSERT_TRUE(mqt.test_empty());
 }
 
-TEST(Queue, subscribe)
-{
+TEST(Queue, subscribe) {
   QueueTest mqt;
   ASSERT_TRUE(mqt.test_subscribe());
 }
 
-TEST(Queue, pushing_message)
-{
+TEST(Queue, pushing_message) {
   QueueTest mqt;
   ASSERT_TRUE(mqt.test_pushing_message());
 }
 
-TEST(Queue, message_broadcasting)
-{
+TEST(Queue, message_broadcasting) {
   QueueTest mqt;
   ASSERT_TRUE(mqt.test_message_broadcasting());
 }
