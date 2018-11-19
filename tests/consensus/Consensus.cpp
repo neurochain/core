b6f50e9bf3edbc98ae94a9561726367a23f133ec
trax Omar Givernaud <omar.givernaud@neurochaintech.io>
2018-11-19 14:32:59 +0100

    [UT] init consensus tests

---
+++ tests/consensus/Consensus.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include <chrono>
+#include <sstream>
+#include <thread>
+#include "common/logger.hpp"
+#include "common/types.hpp"
+#include "consensus/PiiConsensus.hpp"
+
+namespace neuro {
+namespace consensus {
+namespace tests {
+
+TEST(Consensus, trivial_test) {
+  
+}
+
+}  // namespace tests
+}  // consensus
+}  // namespace neuro
