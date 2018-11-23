a8b9687a9e678af0c0791f9e17305cf3fb8f8106
Hermann <hermann.bouly@neurochaintech.io>
2018-11-23 15:05:36 +0100

    Relevant tests for integration.simple_interactions Ref #15

--- tests/integration.cpp
+++ tests/integration.cpp
@@ -20,11 +20,11 @@ using namespace std::chrono_literals;
 
 class Listener {
  private:
-  bool _received_connection0{false};
-  bool _received_connection1{false};
-  bool _received_hello{false};
-  bool _received_world{false};
-  bool _received_deconnection{false};
+  std::size_t _received_connection0{0};
+  std::size_t _received_connection1{0};
+  std::size_t _received_hello{0};
+  std::size_t _received_world{0};
+  std::size_t _received_deconnection{0};
 
  public:
   Listener() {}
@@ -32,35 +32,34 @@ class Listener {
   void handler_hello(const messages::Header &header,
                      const messages::Body &hello) {
     LOG_DEBUG << __FILE__ << ": It entered the handler_hello !!";
-    _received_hello = true;
+    ++_received_hello;
   }
   void handler_world(const messages::Header &header,
                      const messages::Body &hello) {
     LOG_DEBUG << __FILE__ << ": It entered the handler_world !!";
-    _received_world = true;
+    ++_received_world;
   }
   void handler_connection0(const messages::Header &header,
                            const messages::Body &body) {
     LOG_DEBUG << __FILE__ << ": It entered the handler_connection0 !!";
-    _received_connection0 = true;
+    ++_received_connection0;
   }
   void handler_connection1(const messages::Header &header,
                            const messages::Body &body) {
     LOG_DEBUG << __FILE__ << ": It entered the handler_connection1 !!";
-    _received_connection1 = true;
+    ++_received_connection1;
   }
 
   void handler_deconnection(const messages::Header &header,
                             const messages::Body &body) {
-    _received_deconnection = true;
+    ++_received_deconnection;
   }
 
-  bool validated() {
-    return _received_connection0 && _received_connection1 && _received_hello &&
-           _received_world;
-  }
-
-  bool got_deconnection() const { return _received_deconnection; }
+  std::size_t received_connection0() const { return _received_connection0; };
+  std::size_t received_connection1() const { return _received_connection1; };
+  std::size_t received_hello() const { return _received_hello; };
+  std::size_t received_world() const { return _received_world; };
+  std::size_t received_deconnection() const { return _received_deconnection; };
 };
 
 class BotTest {
@@ -88,6 +87,7 @@ TEST(INTEGRATION, simple_interaction) {
 
   messages::Subscriber subscriber0(bot0->queue());
   messages::Subscriber subscriber1(bot1->queue());
+  messages::Subscriber subscriber2(bot2->queue());
   std::cout << __LINE__ << std::endl;
 
   subscriber0.subscribe(
@@ -100,7 +100,22 @@ TEST(INTEGRATION, simple_interaction) {
       [&listener](const messages::Header &header, const messages::Body &body) {
         listener.handler_hello(header, body);
       });
+  subscriber2.subscribe(
+      messages::Type::kHello,
+      [&listener](const messages::Header &header, const messages::Body &body) {
+        listener.handler_hello(header, body);
+      });
+  subscriber0.subscribe(
+      messages::Type::kWorld,
+      [&listener](const messages::Header &header, const messages::Body &body) {
+        listener.handler_world(header, body);
+      });
   subscriber1.subscribe(
+      messages::Type::kWorld,
+      [&listener](const messages::Header &header, const messages::Body &body) {
+        listener.handler_world(header, body);
+      });
+  subscriber2.subscribe(
       messages::Type::kWorld,
       [&listener](const messages::Header &header, const messages::Body &body) {
         listener.handler_world(header, body);
@@ -125,7 +140,13 @@ TEST(INTEGRATION, simple_interaction) {
   std::cout << __LINE__ << std::endl;
 
   std::this_thread::sleep_for(3s);
-  ASSERT_TRUE(listener.validated());
+
+  ASSERT_GT(listener.received_connection0() + listener.received_connection1(), 0);
+  ASSERT_GT(listener.received_connection0(), 0);
+  ASSERT_GT(listener.received_connection1(), 0);
+  ASSERT_GT(listener.received_hello(), 0);
+  ASSERT_GT(listener.received_world(), 0);
+  ASSERT_EQ(listener.received_deconnection(), 0);
 }
 
 TEST(INTEGRATION, neighbors_propagation) {
@@ -147,23 +168,27 @@ TEST(INTEGRATION, neighbors_propagation) {
   auto peers_bot1 = bot1->connected_peers();
   auto peers_bot2 = bot2->connected_peers();
 
-  ASSERT_TRUE(peers_bot0.size() == peers_bot1.size() &&
-              peers_bot1.size() == peers_bot2.size() && peers_bot2.size() == 2);
+  ASSERT_EQ(peers_bot0.size(), peers_bot1.size());
+  ASSERT_EQ(peers_bot1.size(), peers_bot2.size());
+  ASSERT_EQ(peers_bot2.size(), 2);
 
-  ASSERT_TRUE(peers_bot0[0].endpoint() == "127.0.0.1" &&
-              peers_bot0[0].port() == 1338);
-  ASSERT_TRUE(peers_bot0[1].endpoint() == "127.0.0.1" &&
-              peers_bot0[1].port() == 1339);
+  ASSERT_EQ(peers_bot0[0].endpoint(), "127.0.0.1");
+  ASSERT_EQ(peers_bot0[1].endpoint(), "127.0.0.1");
+  ASSERT_TRUE(peers_bot0[0].port() == 1338 || peers_bot0[0].port() == 1339);
+  ASSERT_TRUE(peers_bot0[1].port() == 1338 || peers_bot0[1].port() == 1339);
+  ASSERT_NE(peers_bot0[1].port(), peers_bot0[1].port());
 
-  ASSERT_TRUE(peers_bot1[0].endpoint() == "127.0.0.1" &&
-              peers_bot1[0].port() == 1337);
-  ASSERT_TRUE(peers_bot1[1].endpoint() == "127.0.0.1" &&
-              peers_bot1[1].port() == 1339);
+  ASSERT_EQ(peers_bot1[0].endpoint(), "127.0.0.1");
+  ASSERT_EQ(peers_bot1[1].endpoint(), "127.0.0.1");
+  ASSERT_TRUE(peers_bot1[0].port() == 1337 || peers_bot1[0].port() == 1339);
+  ASSERT_TRUE(peers_bot1[1].port() == 1337 || peers_bot1[1].port() == 1339);
+  ASSERT_NE(peers_bot1[1].port(), peers_bot1[1].port());
 
-  ASSERT_TRUE(peers_bot2[0].endpoint() == "127.0.0.1" &&
-              peers_bot2[0].port() == 1337);
-  ASSERT_TRUE(peers_bot2[1].endpoint() == "127.0.0.1" &&
-              peers_bot2[1].port() == 1338);
+  ASSERT_EQ(peers_bot2[0].endpoint(), "127.0.0.1");
+  ASSERT_EQ(peers_bot2[1].endpoint(), "127.0.0.1");
+  ASSERT_TRUE(peers_bot2[0].port() == 1337 || peers_bot2[0].port() == 1338);
+  ASSERT_TRUE(peers_bot2[1].port() == 1337 || peers_bot2[1].port() == 1338);
+  ASSERT_NE(peers_bot2[1].port(), peers_bot2[1].port());
 }
 
 TEST(INTEGRATION, neighbors_update) {
@@ -180,23 +205,27 @@ TEST(INTEGRATION, neighbors_update) {
   auto peers_bot1 = bot1->connected_peers();
   auto peers_bot2 = bot2->connected_peers();
 
-  ASSERT_TRUE(peers_bot0.size() == peers_bot1.size() &&
-              peers_bot1.size() == peers_bot2.size() && peers_bot2.size() == 2);
+  ASSERT_EQ(peers_bot0.size(), peers_bot1.size());
+  ASSERT_EQ(peers_bot1.size(), peers_bot2.size());
+  ASSERT_EQ(peers_bot2.size(), 2);
 
-  ASSERT_TRUE(peers_bot0[0].endpoint() == "127.0.0.1" &&
-              peers_bot0[0].port() == 1338);
-  ASSERT_TRUE(peers_bot0[1].endpoint() == "127.0.0.1" &&
-              peers_bot0[1].port() == 1339);
+  ASSERT_EQ(peers_bot0[0].endpoint(), "127.0.0.1");
+  ASSERT_EQ(peers_bot0[1].endpoint(), "127.0.0.1");
+  ASSERT_TRUE(peers_bot0[0].port() == 1338 || peers_bot0[0].port() == 1339);
+  ASSERT_TRUE(peers_bot0[1].port() == 1338 || peers_bot0[1].port() == 1339);
+  ASSERT_NE(peers_bot0[0].port(), peers_bot0[1].port());
 
-  ASSERT_TRUE(peers_bot1[0].endpoint() == "127.0.0.1" &&
-              peers_bot1[0].port() == 1337);
-  ASSERT_TRUE(peers_bot1[1].endpoint() == "127.0.0.1" &&
-              peers_bot1[1].port() == 1339);
+  ASSERT_EQ(peers_bot1[0].endpoint(), "127.0.0.1");
+  ASSERT_EQ(peers_bot1[1].endpoint(), "127.0.0.1");
+  ASSERT_TRUE(peers_bot1[0].port() == 1337 || peers_bot1[0].port() == 1339);
+  ASSERT_TRUE(peers_bot1[1].port() == 1337 || peers_bot1[1].port() == 1339);
+  ASSERT_NE(peers_bot1[0].port(), peers_bot1[1].port());
 
-  ASSERT_TRUE(peers_bot2[0].endpoint() == "127.0.0.1" &&
-              peers_bot2[0].port() == 1337);
-  ASSERT_TRUE(peers_bot2[1].endpoint() == "127.0.0.1" &&
-              peers_bot2[1].port() == 1338);
+  ASSERT_EQ(peers_bot2[0].endpoint(), "127.0.0.1");
+  ASSERT_EQ(peers_bot2[1].endpoint(), "127.0.0.1");
+  ASSERT_TRUE(peers_bot2[0].port() == 1337 || peers_bot2[0].port() == 1338);
+  ASSERT_TRUE(peers_bot2[1].port() == 1337 || peers_bot2[1].port() == 1338);
+  ASSERT_NE(peers_bot2[0].port(), peers_bot2[1].port());
 }
 
 TEST(INTEGRATION, terminate_on_bad_version) {
@@ -228,7 +257,7 @@ TEST(INTEGRATION, terminate_on_bad_version) {
 
   std::this_thread::sleep_for(500ms);
 
-  ASSERT_TRUE(listener.got_deconnection());
+  ASSERT_GT(listener.received_deconnection(), 0);
 }
 
 TEST(INTEGRATION, keep_max_connections) {
