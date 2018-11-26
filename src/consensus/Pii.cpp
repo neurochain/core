25bdbbf1a1992dc9cc16c8dd96d17189870adf39
trax Omar Givernaud <omar.givernaud@neurochaintech.io>
2018-11-26 11:46:32 +0100

    [pii] wip impl

---
+++ src/consensus/Pii.cpp
@@ -0,0 +1,21 @@
+#include "consensus/Pii.hpp"
+
+namespace neuro {
+namespace consensus {
+
+bool Transactions::add_block(const messages::Block &block) {
+  for (const auto &transaction : block.transactions()) {
+    _transactions.emplace_back(transaction);
+    for (const auto &input : inputs) {
+      const auto& key_pub = transaction.key_pubs(input.key_id());
+      
+    }
+
+    for (const auto &output : outputs) {
+      
+    }
+  }
+}
+
+}  // consensus
+}  // neuro
