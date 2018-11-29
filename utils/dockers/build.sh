70d29c94da73c78cd7400319245c097f76922d7b
trax Omar Givernaud <omar.givernaud@neurochaintech.io>
2018-11-29 08:21:44 +0000

    [CI] separate docker image for release build Ref #106

--- utils/dockers/build.sh
+++ utils/dockers/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
+docker build  -f Dockerfile.ubuntu-release --tag registry.gitlab.com/neurochaintech/core/ubuntu/release:18.04  . && docker push registry.gitlab.com/neurochaintech/core/ubuntu/release:18.04
 docker build  -f Dockerfile.ubuntu --tag registry.gitlab.com/neurochaintech/core/ubuntu:18.04  . && docker push registry.gitlab.com/neurochaintech/core/ubuntu:18.04
 docker build  -f Dockerfile.fedora --tag registry.gitlab.com/neurochaintech/core/fedora:28  . && docker push registry.gitlab.com/neurochaintech/core/fedora:28
 docker build  -f Dockerfile.debian --tag registry.gitlab.com/neurochaintech/core/debian:sid  . && docker push registry.gitlab.com/neurochaintech/core/debian:sid
