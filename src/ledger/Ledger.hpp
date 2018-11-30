7282cbe9e367d17dfd7273377d9ecf2554b76f74
trax Omar Givernaud <omar.givernaud@neurochaintech.io>
2018-11-30 16:59:14 +0100

    merge feature/118/address-implementation into feature/113/rewrite-new-consensus

--- src/ledger/Ledger.hpp
+++ src/ledger/Ledger.hpp
@@ -305,7 +305,7 @@ class Ledger {
       const messages::Address &address, const messages::NCCSDF &amount,
       const crypto::EccPriv &key_priv,
       const std::optional<messages::NCCSDF> &fees = {}) {
-    messages::Address bot_address (key_priv.make_public_key());
+    messages::Address bot_address(key_priv.make_public_key());
     std::vector<messages::UnspentTransaction> unspent_transactions =
         list_unspent_transactions(bot_address);
 
--- src/messages/Address.hpp
+++ src/messages/Address.hpp
@@ -1,15 +1,15 @@
 #ifndef NEURO_SRC_MESSAGES_ADDRESS_HPP
 #define NEURO_SRC_MESSAGES_ADDRESS_HPP
 
-#include "messages/Hasher.hpp"
-#include "common/logger.hpp"
 #include "common.pb.h"
+#include "common/logger.hpp"
+#include "messages/Hasher.hpp"
 
 namespace neuro {
 namespace messages {
 
-class Address: public _Address {
-private:
+class Address : public _Address {
+ private:
   int hash_size = 10;
   int checksum_size = 2;
 
@@ -20,35 +20,33 @@ private:
     key.append(checksum);
     set_data(key.data(), key.size());
   }
-  
-public:
-  Address(const messages::KeyPub &key_pub) {
-    load(key_pub);
-  }
+
+ public:
+  Address(const messages::KeyPub &key_pub) { load(key_pub); }
 
   Address(const crypto::EccPub &ecc_pub) {
     messages::KeyPub key_pub;
     ecc_pub.save(&key_pub);
     load(key_pub);
   }
-  
+
   Address(const Address &address) {}
   Address &operator=(const Address &) {}
-  
+
   bool verify() const {
-    if(!has_data()) {
+    if (!has_data()) {
       return false;
     }
 
-    if(data().size() != (hash_size + checksum_size)) {
+    if (data().size() != (hash_size + checksum_size)) {
       return false;
     }
-    
+
     const Buffer key(data().substr(0, hash_size));
     const Buffer checksum_from_key(data().substr(hash_size, checksum_size));
     auto checksum_computed = crypto::hash_sha3_256(key);
-    
-    if(checksum_computed != checksum_from_key) {
+
+    if (checksum_computed != checksum_from_key) {
       LOG_INFO << "Key checksum error";
       return false;
     }
@@ -56,12 +54,9 @@ public:
     return true;
   }
 
-  operator bool () const {
-    return verify();
-  }
+  operator bool() const { return verify(); }
 };
 
-  
 }  // namespace messages
 }  // namespace neuro
 
