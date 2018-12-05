947977a2034a6ad516cfb5b84658c46126ba5769
Hermann <hermann.bouly@neurochaintech.io>
2018-12-05 11:33:46 +0100

    Test integration.terminate_on_bad_version restored Ref #103

--- tests/integration.cpp
+++ tests/integration.cpp
@@ -243,38 +243,43 @@ TEST(INTEGRATION, neighbors_update) {
   ASSERT_NE(peers_bot2[0].port(), peers_bot2[1].port());
 }
 
-// TEST(INTEGRATION, terminate_on_bad_version) {
-//   auto bot0 = std::make_shared<neuro::Bot>("integration_propagation0.json");
-//   auto bot1 = std::make_shared<neuro::Bot>("integration_propagation1.json");
+TEST(INTEGRATION, terminate_on_bad_version) {
+  Listener listener;
+  std::shared_ptr<messages::Subscriber> subscriber0;
+  {
+    Path config_path0("integration_propagation0.json");
+    messages::config::Config config0(config_path0);
+    auto bot0 = std::make_shared<Bot>(config0);
+    Path config_path1("integration_propagation1.json");
+    messages::config::Config config1(config_path1);
+    auto bot1 = std::make_shared<Bot>(config1);
 
-//   std::this_thread::sleep_for(500ms);
+    std::this_thread::sleep_for(500ms);
 
-//   Listener listener;
-//   messages::Subscriber subscriber0(bot0->queue());
+    subscriber0 = std::make_shared<messages::Subscriber>(bot0->queue());
 
-//   subscriber0.subscribe(
-//       messages::Type::kConnectionClosed,
-//       [&listener](const messages::Header &header, const messages::Body &body)
-//       {
-//         listener.handler_deconnection(header, body);
-//       });
+    subscriber0->subscribe(messages::Type::kConnectionClosed,
+                          [&listener](const messages::Header &header,
+                                      const messages::Body &body) {
+                            listener.handler_deconnection(header, body);
+                          });
 
-//   auto peers_bot0 = bot0->connected_peers();
+    auto peers_bot0 = bot0->connected_peers();
 
-//   ASSERT_TRUE(peers_bot0[0].endpoint() == "127.0.0.1" &&
-//               peers_bot0[0].port() == 1338);
+    ASSERT_TRUE(peers_bot0[0].endpoint() == "127.0.0.1" &&
+                peers_bot0[0].port() == 1338);
 
-//   auto msg = std::make_shared<messages::Message>();
-//   msg->add_bodies()->mutable_get_peers();
-//   auto header = msg->mutable_header();
-//   messages::fill_header(header);
-//   header->set_version(neuro::MessageVersion + 100);
-//   bot0->networking()->send(msg, neuro::networking::ProtocolType::PROTOBUF2);
+    auto msg = std::make_shared<messages::Message>();
+    msg->add_bodies()->mutable_get_peers();
+    auto header = msg->mutable_header();
+    messages::fill_header(header);
+    header->set_version(neuro::MessageVersion + 100);
+    bot0->networking()->send(msg, neuro::networking::ProtocolType::PROTOBUF2);
+  }
+  std::this_thread::sleep_for(500ms);
 
-//   std::this_thread::sleep_for(500ms);
-
-//   ASSERT_GT(listener.received_deconnection(), 0);
-// }
+  ASSERT_GT(listener.received_deconnection(), 0);
+}
 
 // TEST(INTEGRATION, keep_max_connections) {
 //   auto bot0 = std::make_shared<neuro::Bot>("integration_keepmax0.json");
