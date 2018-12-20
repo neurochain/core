4bfe5d33da7561631580a2255ec7c4315c8c99cf
trax Omar Givernaud <omar.givernaud@neurochaintech.io>
2018-12-20 13:27:49 +0000

    [ci] troubleshoot ci Ref #106

--- tests/integration.cpp
+++ tests/integration.cpp
@@ -1,10 +1,24 @@
 #include <gtest/gtest.h>
+#include "ledger/LedgerMongodb.hpp"
 
 namespace neuro {
 namespace tests {
 
 
-TEST(foo, bar) {}
+TEST(foo, bar) {
+  ASSERT_TRUE(true);
+}
+
+TEST(bar, foo) {
+  const std::string config_str =
+      "{"
+      "  \"url\": \"mongodb://mongo:27017\","
+      "  \"dbName\": \"test_ledger\","
+      "  \"block0Format\": \"PROTO\","
+      "  \"block0Path\": \"./data.0.testnet\""
+      "}";
+  auto ledger = std::make_shared<::neuro::ledger::LedgerMongodb>(_config);
+}
 
 }  // namespace tests
 }  // namespace neuro
