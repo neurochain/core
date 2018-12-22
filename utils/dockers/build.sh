ecb9e5de4654b9f8ac51e50d13017cb8ee47d013
trax Omar Givernaud <omar.givernaud@neurochaintech.io>
2018-12-22 13:37:10 +0100

    [ci] using ubuntu LTS for dockers images Ref #127

--- utils/dockers/build.sh
+++ utils/dockers/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -ex
-docker build  -f Dockerfile.ubuntu-release --tag registry.gitlab.com/neurochaintech/core/ubuntu/release:18.10  . && docker push registry.gitlab.com/neurochaintech/core/ubuntu/release:18.10
-docker build  -f Dockerfile.ubuntu --tag registry.gitlab.com/neurochaintech/core/ubuntu:18.10  . && docker push registry.gitlab.com/neurochaintech/core/ubuntu:18.10
-
+docker build -f Dockerfile.ubuntu-release --tag registry.gitlab.com/neurochaintech/core/ubuntu/release:18.04  . && docker push registry.gitlab.com/neurochaintech/core/ubuntu/release:18.04
+docker build -f Dockerfile.ubuntu --tag registry.gitlab.com/neurochaintech/core/ubuntu:18.04  . && docker push registry.gitlab.com/neurochaintech/core/ubuntu:18.04
