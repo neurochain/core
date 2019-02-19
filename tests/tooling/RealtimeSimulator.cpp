773f297321ffff93f5ebedc694caef3a681d3d12
Olivier Lauzanne <olivier.lauzanne@neurochaintech.io>
2019-02-19 10:35:28 +0100

    Fix tests #Ref #113

--- tests/tooling/RealtimeSimulator.cpp
+++ tests/tooling/RealtimeSimulator.cpp
@@ -32,7 +32,7 @@ class RealtimeSimulator : public testing::Test {
     messages::Block block0;
     ASSERT_TRUE(ledger->get_block(0, &block0));
     uint64_t block0_timestamp = block0.header().timestamp().data();
-    const auto nb_transactions = 3;
+    const auto nb_transactions = 2;
     for (uint32_t i = 1; i < 3 * consensus->config.blocks_per_assembly; i++) {
       for (auto j = 0; j < nb_transactions; j++) {
         int sender_index = rand() % nb_keys;
