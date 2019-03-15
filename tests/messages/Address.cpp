57d14d5c6fccddb1f042142098347c21a3562a70
Olivier Lauzanne <olivier.lauzanne@neurochaintech.io>
2019-03-15 11:35:41 +0100

    Add a test for the Address class Ref #135

--- tests/messages/Address.cpp
+++ tests/messages/Address.cpp
@@ -1,6 +1,7 @@
 #include "messages/Address.hpp"
 #include <cryptopp/hex.h>
 #include <gtest/gtest.h>
+#include "crypto/EccPub.hpp"
 
 namespace neuro {
 namespace messages {
@@ -16,6 +17,22 @@ TEST(Address, encode_base58) {
   ASSERT_EQ(encode_base58(57 * 58 + 57, ""), "zz");
   ASSERT_EQ(encode_base58(58 * 58, ""), "211");
   ASSERT_EQ(encode_base58(0, "TITI"), "TITI1");
+}
+
+TEST(Address, address) {
+  crypto::EccPub ecc_pub;
+  messages::KeyPub key_pub;
+  ecc_pub.save(&key_pub);
+  Address address0(ecc_pub);
+  Address address1(key_pub);
+  auto address2 = Address::random();
+  for (const auto &address : {address0, address1, address2}) {
+    ASSERT_EQ(address.data().size(), 34);
+    ASSERT_EQ(address.data()[0], 'N');
+  }
+  ASSERT_EQ(address0, address1);
+  ASSERT_NE(address0, address2);
+  ASSERT_NE(address1, address2);
 }
 
 }  // namespace test
