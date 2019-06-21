1ef7608b29611b263cf2cf3b383970a3f38906df
nepta <damien.martel@neurochaintech.io>
2019-06-21 16:17:52 +0200

    [debug] don't shuffle peers list for integration test Ref #140

--- src/messages/Peers.hpp
+++ src/messages/Peers.hpp
@@ -36,6 +36,9 @@ class Peers {
     void shuffle() {
       std::mt19937 g(_rd());
       std::shuffle(_peers.begin(), _peers.end(), g);
+#ifndef NDEBUG
+      std::sort(_peers.begin(), _peers.end());
+#endif
       _it = _peers.begin();
     }
 
