bf17a4cdecf90dfa17e8fe76cfef8d2348e491d3
nepta <damien.martel@neurochaintech.io>
2019-06-24 15:19:15 +0200

    [API] removed InstallSignalHandler method usage Ref #148

--- src/api/Rest.hpp
+++ src/api/Rest.hpp
@@ -25,8 +25,7 @@ class Rest : public Api {
   void init() {
     auto opts = Http::Endpoint::options()
                 .threads(1)
-                .maxPayload(1048576)  // 1Mio
-                .flags(Tcp::Options::InstallSignalHandler);
+                .maxPayload(1048576);  // 1Mio
     httpEndpoint->init(opts);
     setupRoutes();
     start();
