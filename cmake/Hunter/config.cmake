ffbda854c5644d560dee0094d3e4e630c7d32318
Olivier Lauzanne <olivier.lauzanne@neurochaintech.io>
2019-07-08 19:02:26 +0200

    Fix docker image Ref #102

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -6,5 +6,5 @@ hunter_config(
     "https://github.com/hunter-packages/cryptopp/archive/v5.6.5-p0.tar.gz"
     SHA1
     4258c9b49c48c433c4aa63629bc896ac9a3902e3
-    CMAKE_ARGS "CMAKE_CXX_FLAGS='-march=x86-64';CRYPTOPP_CROSS_COMPILE=On;CRYPTOPP_NATIVE_ARCH=Off"
+    CMAKE_ARGS "CMAKE_CXX_FLAGS='-march=x86-64';CRYPTOPP_CROSS_COMPILE=On;CRYPTOPP_NATIVE_ARCH=Off;CRYPTOPP_CROSS_COMPILE=On"
 )
