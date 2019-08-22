420810c4354cfd81a7534388eb60386636bdf607
trax Omar Givernaud <o.givernaud@gmail.com>
2019-08-22 15:54:18 +0200

    [version] bump from 3 to 4 Ref #179

--- src/common/types.hpp
+++ src/common/types.hpp
@@ -63,7 +63,7 @@ using Duration = std::chrono::seconds;
 using Endpoint = std::string;
 using Port = uint16_t;
 using Ports = std::vector<Port>;
-const int32_t MessageVersion = 3;
+const int32_t MessageVersion = 4;
 const int32_t MESSAGE_TTL = 60;
 const int32_t MAX_MESSAGE_SIZE = 256 * 1024;
 
