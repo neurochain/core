b46e62c676fc296dccc5145bc0645e3df01f948d
nepta <damien.martel@neurochaintech.io>
2019-09-04 10:12:35 +0200

    [consensus] use condition variable instead of sleep to speed up bot destruction Ref #181

--- src/consensus/Consensus.hpp
+++ src/consensus/Consensus.hpp
@@ -7,6 +7,7 @@
 #include <mutex>
 #include <thread>
 #include <unordered_set>
+#include <condition_variable>
 
 #include "common.pb.h"
 #include "consensus.pb.h"
@@ -44,17 +45,17 @@ class Consensus {
   const std::vector<crypto::Ecc> &_keys;
   std::vector<messages::_KeyPub> _key_pubs;
   const PublishBlock _publish_block;
-  std::atomic<bool> _stop_compute_pii;
   std::thread _compute_pii_thread;
   std::vector<std::pair<messages::BlockHeight, KeyPubIndex>> _heights_to_write;
   std::mutex _heights_to_write_mutex;
   std::recursive_mutex _verify_blocks_mutex;
   std::optional<messages::AssemblyID> _previous_assembly_id;
   std::optional<messages::AssemblyHeight> _current_assembly_height;
-  std::atomic<bool> _stop_update_heights;
   std::thread _update_heights_thread;
-  std::atomic<bool> _stop_miner;
   std::thread _miner_thread;
+  std::condition_variable _is_stoped_cv;
+  std::mutex _is_stoped_mutex;
+  bool _is_stoped;
 
   bool check_outputs(
       const messages::TaggedTransaction &tagged_transaction) const;
--- tests/consensus/Consensus.cpp
+++ tests/consensus/Consensus.cpp
@@ -121,7 +121,7 @@ class Consensus : public testing::Test {
     std::this_thread::sleep_for(consensus->config().compute_pii_sleep);
 
     // Wait for the computation to be finished
-    consensus->_stop_compute_pii = true;
+    consensus->_is_stoped = true;
     if (consensus->_compute_pii_thread.joinable()) {
       consensus->_compute_pii_thread.join();
     }
