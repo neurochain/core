90c93676086f6001eaadcb2c019f0fe67085615d
trax Omar Givernaud <o.givernaud@gmail.com>
2019-09-18 16:26:29 +0200

    moar logs

--- src/Bot.cpp
+++ src/Bot.cpp
@@ -75,7 +75,7 @@ void Bot::handler_get_block(const messages::Header &header,
 void Bot::handler_block(const messages::Header &header,
                         const messages::Body &body) {
   // bool reply_message = header.has_request_id();
-  std::cout << "trax> got block " << body.block().header().id() << std::endl;
+  LOG_TRACE;
   if (!_consensus->add_block(body.block())) {
     LOG_WARNING << "Consensus rejected block";
     return;
@@ -85,7 +85,7 @@ void Bot::handler_block(const messages::Header &header,
   if (header.has_request_id()) {
     auto got = _request_ids.find(header.request_id());
     if (got != _request_ids.end()) {
-      std::cout << "trax> dropping block " << body.block().header().id() << std::endl;
+      LOG_WARNING << "Reply rejected block " << body.block().header().id();
       return;
     }
   }
@@ -95,7 +95,7 @@ void Bot::handler_block(const messages::Header &header,
   auto id = messages::fill_header(header_reply);
   message.add_bodies()->mutable_block()->CopyFrom(body.block());
   send_all(message);
-
+  LOG_DEBUG << "Broadcasting block " << body.block().header().id().data();
   _request_ids.insert(id);
 }
 
@@ -125,15 +125,13 @@ bool Bot::update_ledger(const std::optional<messages::Hash> &missing_block) {
   if (!send_one(*message)) {
     LOG_INFO << "no bot found to ask block " << *message;
   }
-  std::cout << "trax> asking for block " << *missing_block << "-------" << std::endl;
 
   _request_ids.insert(idheader);
   return false;
 }
 
 void Bot::update_ledger() {
-  const auto missing_blocks = _ledger->missing_blocks();
-  for (const auto &missing_block : missing_blocks) {
+  for (const auto &missing_block : _ledger->missing_blocks()) {
     update_ledger(missing_block);
   }
 }
