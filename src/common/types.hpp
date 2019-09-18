ed6b226662808be63a9d5630880280359e20f05c
Olivier Lauzanne <olivier.lauzanne@neurochaintech.io>
2019-09-18 12:06:18 +0200

    Bump version

--- src/common/types.hpp
+++ src/common/types.hpp
@@ -64,7 +64,7 @@ using Duration = std::chrono::seconds;
 using Endpoint = std::string;
 using Port = uint16_t;
 using Ports = std::vector<Port>;
-const int32_t MessageVersion = 5;
+const int32_t MessageVersion = 6;
 const int32_t MESSAGE_TTL = 60;
 const int32_t MAX_MESSAGE_SIZE = 256 * 1024;
 
