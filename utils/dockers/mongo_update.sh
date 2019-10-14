165da5a80669eb70d5edf4d8a94f4722b0d0f666
trax Omar Givernaud <o.givernaud@gmail.com>
2019-10-14 12:00:48 +0200

    I am a boulet Ref #179

--- utils/dockers/mongo_update.sh
+++ utils/dockers/mongo_update.sh
@@ -4,7 +4,7 @@ dir=$(mktemp -d)
 cd $dir
 
 wget https://api.testnet.neurochaintech.io/testnet.tar
-tar xf testnet.tar.gz
+tar xf testnet.tar
 mongorestore --drop  --uri mongodb://mongo:27017 testnet 
 #rm -r $dir
 popd 
