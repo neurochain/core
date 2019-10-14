aa5cc4904d72ac43d7620bbce0279f45df038347
trax Omar Givernaud <o.givernaud@gmail.com>
2019-10-14 11:57:15 +0200

    [mongo_update] no more gz, compression is taking too long on server Ref #179

--- utils/dockers/mongo_update.sh
+++ utils/dockers/mongo_update.sh
@@ -3,8 +3,8 @@ pushd $(pwd)
 dir=$(mktemp -d)
 cd $dir
 
-wget https://api.testnet.neurochaintech.io/testnet.tar.gz
+wget https://api.testnet.neurochaintech.io/testnet.tar
 tar xf testnet.tar.gz
 mongorestore --drop  --uri mongodb://mongo:27017 testnet 
-rm -r $dir
+#rm -r $dir
 popd 
