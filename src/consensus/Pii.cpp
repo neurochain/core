3c86d1bed4dd159988a646f14179faf26f4fa90e
Olivier Lauzanne <olivier.lauzanne@neurochaintech.io>
2019-11-04 10:35:44 +0100

    Add const #233

--- src/consensus/Pii.cpp
+++ src/consensus/Pii.cpp
@@ -6,6 +6,8 @@
 namespace neuro {
 namespace consensus {
 
+const double NCC_SUBDIVISIONS = 1E9;
+
 Double Pii::enthalpy_n() const { return 1; }
 
 Double Pii::enthalpy_c() const { return 1; }
@@ -44,7 +46,7 @@ bool Pii::add_transaction(const messages::Transaction &transaction,
 
         // We want the enthalpy in NCC and not in the smallest unit
         const Double raw_enthalpy =
-            output_ratio * enthalpy_spent_transaction / 1E9;
+            output_ratio * enthalpy_spent_transaction / NCC_SUBDIVISIONS;
 
         // TODO reference to a pdf in english that contains the formula
         const Double enthalpy =
