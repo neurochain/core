472d4e6abdf2f2db8615242a76745b6cd9800109
Olivier Lauzanne <olivier.lauzanne@neurochaintech.io>
2019-11-04 18:04:28 +0100

    Test the influence of the previous PII #229

--- tests/consensus/Pii.cpp
+++ tests/consensus/Pii.cpp
@@ -9,16 +9,17 @@ namespace neuro {
 namespace consensus {
 namespace tests {
 
-void almost_eq(Double a, Double b) {
-  ASSERT_GT(a + 0.001, b);
-  ASSERT_LT(a - 0.001, b);
+bool almost_eq(Double a, Double b) {
+  EXPECT_GT(a + 0.001, b);
+  EXPECT_LT(a - 0.001, b);
+  return a + 0.001 > b && a - 0.001 < b;
 }
 
 class Pii : public testing::Test {
  public:
   const std::string db_url = "mongodb://mongo:27017";
   const std::string db_name = "test_pii";
-  const messages::NCCAmount ncc_block0 = messages::NCCAmount(1E12);
+  const messages::NCCAmount ncc_block0 = messages::NCCAmount(1E15);
   const int nb_keys = 20;
   tooling::Simulator simulator;
   std::shared_ptr<neuro::ledger::LedgerMongodb> ledger;
@@ -164,13 +165,13 @@ class Pii : public testing::Test {
     auto lambda = this->pii.enthalpy_lambda();
     auto n = this->pii.enthalpy_n();
     auto c = this->pii.enthalpy_c();
-    Double raw_enthalpy = ncc_block0.value() / 10;
+    Double raw_enthalpy = ncc_block0.value() / 10 / NCC_SUBDIVISIONS;
     auto enthalpy =
         mpfr::pow(mpfr::log(lambda * c * raw_enthalpy /
                             simulator.consensus->config().blocks_per_assembly),
                   n);
     auto expected_pii = -enthalpy * mpfr::log2(Double(1.0) / 10);
-    almost_eq(pii, expected_pii);
+    ASSERT_TRUE(almost_eq(pii, expected_pii));
 
     ledger->get_pii(simulator.keys.at(1).key_pub(), assembly.id(), &pii);
     ASSERT_EQ(pii, 1);
@@ -246,7 +247,7 @@ class Pii : public testing::Test {
     ASSERT_GT(pii, 1);
 
     // The coins are sent at block1 so the raw_enthalpy is just the amount sent
-    Double raw_enthalpy = ncc_block0.value() / 10;
+    Double raw_enthalpy = ncc_block0.value() / 10 / NCC_SUBDIVISIONS;
     auto lambda = this->pii.enthalpy_lambda();
     auto n = this->pii.enthalpy_n();
     auto c = this->pii.enthalpy_c();
@@ -255,7 +256,7 @@ class Pii : public testing::Test {
                             simulator.consensus->config().blocks_per_assembly),
                   n);
     auto expected_pii = -enthalpy * mpfr::log2(Double(1.0) / 10);
-    almost_eq(pii, expected_pii);
+    ASSERT_TRUE(almost_eq(pii, expected_pii));
 
     for (int i = 1; i < 20; i++) {
       ledger->get_pii(simulator.keys.at(i).key_pub(), assembly.id(), &pii);
@@ -288,6 +289,253 @@ class Pii : public testing::Test {
     for (int i = 0; i < 20; i++) {
       ledger->get_pii(simulator.keys.at(i).key_pub(), assembly.id(), &pii);
       ASSERT_EQ(pii, 1);
+    }
+  }
+
+  void test_send_receive_pii() {
+    messages::TaggedBlock last_block;
+    ledger->get_last_block(&last_block);
+    ASSERT_EQ(last_block.block().header().height(), 0);
+
+    // The key0 will receive money from the keys 10 to 19
+    for (int i = 10; i < 20; i++) {
+      const auto &ecc = simulator.keys.at(i);
+      auto transaction =
+          ledger->send_ncc(ecc.key_priv(), simulator.keys.at(0).key_pub(), 0.1);
+      simulator.consensus->add_transaction(transaction);
+    }
+
+    for (int i = 1; i < 6; i++) {
+      if (i == 2) {
+        // At block 2 send the money back
+        for (int i = 10; i < 20; i++) {
+          const auto &ecc = simulator.keys.at(i);
+          auto transaction = ledger->send_ncc(simulator.keys.at(0).key_priv(),
+                                              ecc.key_pub(), 0.1);
+          simulator.consensus->add_transaction(transaction);
+        }
+      }
+
+      ledger->get_last_block(&last_block);
+      ASSERT_EQ(last_block.block().header().height(), i - 1);
+      auto block = simulator.new_block(last_block);
+      simulator.consensus->add_block(block);
+      ASSERT_EQ(block.header().height(), i);
+
+      if (i <= 2) {
+        ASSERT_EQ(block.transactions_size(), 10);
+      } else {
+        ASSERT_EQ(block.transactions_size(), 0);
+      }
+    }
+
+    // Compute the assembly piis
+    messages::Assembly assembly;
+    ASSERT_TRUE(ledger->get_assembly(0, &assembly));
+    ASSERT_TRUE(simulator.consensus->compute_assembly_pii(assembly));
+
+    // Check the Piis
+    Double pii;
+    ledger->get_pii(simulator.keys.at(0).key_pub(), assembly.id(), &pii);
+    ASSERT_GT(pii, 1);
+
+    // The coins are sent at block1 so the raw_enthalpy is just the amount sent
+    Double raw_enthalpy_in = ncc_block0.value() / 10 / NCC_SUBDIVISIONS;
+    Double average_age = 1.5;
+    Double raw_enthalpy_out =
+        2 * average_age * ncc_block0.value() / 10 / NCC_SUBDIVISIONS;
+    auto lambda = this->pii.enthalpy_lambda();
+    auto n = this->pii.enthalpy_n();
+    auto c = this->pii.enthalpy_c();
+    auto enthalpy_in =
+        mpfr::pow(mpfr::log(lambda * c * raw_enthalpy_in /
+                            simulator.consensus->config().blocks_per_assembly),
+                  n);
+    auto enthalpy_out =
+        mpfr::pow(mpfr::log(lambda * c * raw_enthalpy_out /
+                            simulator.consensus->config().blocks_per_assembly),
+                  n);
+    auto enthalpy = enthalpy_in + enthalpy_out;
+    auto expected_pii = -enthalpy * mpfr::log2(Double(1.0) / 10);
+    ASSERT_TRUE(almost_eq(pii, expected_pii));
+
+    for (int i = 1; i < 20; i++) {
+      ledger->get_pii(simulator.keys.at(i).key_pub(), assembly.id(), &pii);
+      ASSERT_EQ(pii, 1);
+    }
+
+    // Run an assembly with 1 transaction and check that the piis are back to 1
+    auto transaction = ledger->send_ncc(simulator.keys.at(1).key_priv(),
+                                        simulator.keys.at(2).key_pub(), 0.1);
+    simulator.consensus->add_transaction(transaction);
+
+    for (int i = 6; i < 11; i++) {
+      ledger->get_last_block(&last_block);
+      ASSERT_EQ(last_block.block().header().height(), i - 1);
+      auto block = simulator.new_block(last_block);
+      simulator.consensus->add_block(block);
+      ASSERT_EQ(block.header().height(), i);
+      if (i == 6) {
+        ASSERT_EQ(block.transactions_size(), 1);
+      } else {
+        ASSERT_EQ(block.transactions_size(), 0);
+      }
+    }
+
+    // Compute the assembly piis
+    ASSERT_TRUE(ledger->get_assembly(1, &assembly));
+    ASSERT_TRUE(simulator.consensus->compute_assembly_pii(assembly));
+
+    // Check the Pii
+    for (int i = 0; i < 20; i++) {
+      ledger->get_pii(simulator.keys.at(i).key_pub(), assembly.id(), &pii);
+      ASSERT_EQ(pii, 1);
+    }
+  }
+
+  void test_previous_pii() {
+    messages::TaggedBlock last_block;
+    ledger->get_last_block(&last_block);
+    ASSERT_EQ(last_block.block().header().height(), 0);
+
+    // The key 0 sends 10NCC its coins to the keys 10 to 19
+    for (int i = 10; i < 20; i++) {
+      const auto &ecc = simulator.keys.at(i);
+
+      auto transaction =
+          ledger->send_ncc(simulator.keys.at(0).key_priv(), ecc.key_pub(),
+                           messages::NCCAmount(1E11));
+      simulator.consensus->add_transaction(transaction);
+    }
+
+    for (int i = 1; i < 6; i++) {
+      ledger->get_last_block(&last_block);
+      ASSERT_EQ(last_block.block().header().height(), i - 1);
+      auto block = simulator.new_block(last_block);
+      simulator.consensus->add_block(block);
+      ASSERT_EQ(block.header().height(), i);
+      if (i == 1) {
+        ASSERT_EQ(block.transactions_size(), 10);
+      } else {
+        ASSERT_EQ(block.transactions_size(), 0);
+      }
+    }
+
+    // Compute the assembly piis
+    messages::Assembly assembly;
+    ASSERT_TRUE(ledger->get_assembly(0, &assembly));
+    ASSERT_TRUE(simulator.consensus->compute_assembly_pii(assembly));
+
+    // Check the Piis
+    Double pii;
+    ledger->get_pii(simulator.keys.at(0).key_pub(), assembly.id(), &pii);
+    // ASSERT_GT(pii, 1);
+
+    // The coins are sent at block1 so the raw_enthalpy is just the amount
+    // sent
+    auto lambda = this->pii.enthalpy_lambda();
+    auto n = this->pii.enthalpy_n();
+    auto c = this->pii.enthalpy_c();
+    Double raw_enthalpy = 1E11 / NCC_SUBDIVISIONS;
+    auto enthalpy =
+        mpfr::pow(mpfr::log(lambda * c * raw_enthalpy /
+                            simulator.consensus->config().blocks_per_assembly),
+                  n);
+    auto expected_pii = -enthalpy * mpfr::log2(Double(1.0) / 10);
+    ASSERT_TRUE(almost_eq(pii, expected_pii));
+
+    // Store the pii for later
+    Double previous_pii = pii;
+
+    ledger->get_pii(simulator.keys.at(1).key_pub(), assembly.id(), &pii);
+    ASSERT_EQ(pii, 1);
+
+    for (int i = 1; i < 20; i++) {
+      ledger->get_pii(simulator.keys.at(i).key_pub(), assembly.id(), &pii);
+      ASSERT_EQ(pii, 1);
+    }
+
+    // Now do the same thing again to see the influence of the previous PII
+
+    // The key 0 sends 10NCC its coins to the keys 10 to 19
+    for (int i = 10; i < 20; i++) {
+      const auto &ecc = simulator.keys.at(i);
+
+      auto transaction =
+          ledger->send_ncc(simulator.keys.at(0).key_priv(), ecc.key_pub(),
+                           messages::NCCAmount(1E11));
+      simulator.consensus->add_transaction(transaction);
+    }
+
+    for (int i = 6; i < 11; i++) {
+      ledger->get_last_block(&last_block);
+      ASSERT_EQ(last_block.block().header().height(), i - 1);
+      auto block = simulator.new_block(last_block);
+      simulator.consensus->add_block(block);
+      ASSERT_EQ(block.header().height(), i);
+      if (i == 6) {
+        ASSERT_EQ(block.transactions_size(), 10);
+      } else {
+        ASSERT_EQ(block.transactions_size(), 0);
+      }
+    }
+
+    // Compute the assembly piis
+    ASSERT_TRUE(ledger->get_assembly(1, &assembly));
+    ASSERT_TRUE(simulator.consensus->compute_assembly_pii(assembly));
+
+    // Check the Piis
+    ledger->get_pii(simulator.keys.at(0).key_pub(), assembly.id(), &pii);
+    ASSERT_GT(pii, 1);
+
+    // The coins are sent at block1 so the raw_enthalpy is just the amount
+    // sent
+    Double coins_age = 1 + simulator.consensus->config().blocks_per_assembly;
+    raw_enthalpy = coins_age * 1E11 / NCC_SUBDIVISIONS;
+    enthalpy =
+        mpfr::pow(mpfr::log(previous_pii * lambda * c * raw_enthalpy /
+                            simulator.consensus->config().blocks_per_assembly),
+                  n);
+    expected_pii = -enthalpy * mpfr::log2(Double(1.0) / 10);
+    ASSERT_TRUE(almost_eq(pii, expected_pii));
+
+    ledger->get_pii(simulator.keys.at(1).key_pub(), assembly.id(), &pii);
+    ASSERT_EQ(pii, 1);
+
+    for (int i = 1; i < 20; i++) {
+      ledger->get_pii(simulator.keys.at(i).key_pub(), assembly.id(), &pii);
+      ASSERT_EQ(pii, 1);
+    }
+
+    // Run an assembly with 1 transaction and check that the piis are back to
+    // 1
+    auto transaction = ledger->send_ncc(simulator.keys.at(1).key_priv(),
+                                        simulator.keys.at(2).key_pub(), 0.1);
+    simulator.consensus->add_transaction(transaction);
+
+    for (int i = 11; i < 16; i++) {
+      ledger->get_last_block(&last_block);
+      ASSERT_EQ(last_block.block().header().height(), i - 1);
+      auto block = simulator.new_block(last_block);
+      simulator.consensus->add_block(block);
+      ASSERT_EQ(block.header().height(), i);
+      if (i == 11) {
+        ASSERT_EQ(block.transactions_size(), 1);
+      } else {
+        ASSERT_EQ(block.transactions_size(), 0);
+      }
+    }
+
+    // Compute the assembly piis
+    ASSERT_TRUE(ledger->get_assembly(2, &assembly));
+    ASSERT_TRUE(simulator.consensus->compute_assembly_pii(assembly));
+
+    // Check the Pii
+    for (int i = 0; i < 20; i++) {
+      ledger->get_pii(simulator.keys.at(i).key_pub(), assembly.id(), &pii);
+
+      // We do an almost_eq because of the integrity
+      ASSERT_TRUE(almost_eq(pii, 1));
     }
   }
 };
@@ -324,6 +572,10 @@ TEST_F(Pii, add_block) { test_add_block(); }
 TEST_F(Pii, send_pii) { test_send_pii(); }
 
 TEST_F(Pii, receive_pii) { test_receive_pii(); }
+
+TEST_F(Pii, send_receive_pii) { test_send_receive_pii(); }
+
+TEST_F(Pii, previous_pii) { test_previous_pii(); }
 
 }  // namespace tests
 }  // namespace consensus
