c55227b0c1bf7227c8b77d6ee5228f34de6bb668
trax Omar Givernaud <o.givernaud@gmail.com>
2020-01-16 20:41:08 +0100

    [hunter] upgrade hunter Ref #249

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -7,3 +7,34 @@ hunter_config(
     SHA1
     38a70c9ba970cc862b5cca0010fffdd4e56afcae
 )
+
+hunter_config(
+    mongo-c-driver
+    VERSION
+    1.12.0
+    URL
+    "https://github.com/mongodb/mongo-c-driver/archive/1.12.0.tar.gz"
+    SHA1
+    579c0f0663cd5d7fa2b79fcea99f39dfbac5fb81
+)
+
+hunter_config(
+    mongo-cxx-driver
+    VERSION
+    3.3.2
+    URL
+    "https://github.com/trax44/mongo-cxx-driver/archive/releases/v3.3.zip"
+    SHA1
+    1cd204525f3c6a9bc58f2df4db8c53b0225b21e3
+)
+
+
+hunter_config(
+  pistache
+  VERSION
+  97d0cb3b751f690460b9ceb6d39464ac23d45f1e
+  URL
+  "https://github.com/oktal/pistache/archive/97d0cb3b751f690460b9ceb6d39464ac23d45f1e.zip"
+  SHA1
+  346cb785fc90bf062b42246e87cfe35a26192275
+  )
