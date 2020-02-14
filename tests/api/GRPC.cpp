2307fa82caffded2a2754587bc807b027d108d6d
nepta <nepta23@gmail.com>
2020-02-14 11:02:12 +0100

    [test] added a light grpc client to display bot response #249

---
+++ tests/api/GRPC.cpp
@@ -0,0 +1,24 @@
+#include <grpc/grpc.h>
+#include <grpcpp/channel.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/security/credentials.h>
+#include "messages/rest.grpc.pb.h"
+#include "messages/Message.hpp"
+
+namespace neuro::api::test {
+}  // namespace neuro::api::test
+
+using namespace neuro;
+
+int main() {
+  auto channel = CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
+  auto stub = messages::Monitoring::NewStub(channel);
+  grpc::ClientContext ctx;
+  messages::Status response;
+  stub->get_status(&ctx, google::protobuf::Empty(), &response);
+  std::cerr << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__ << ": "
+            << to_json(response, true)
+            << std::endl;
+  return 0;
+}
