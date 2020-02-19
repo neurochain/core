9ab032abd7446c81f867bce0ac6f7b7201c94dc1
nepta <nepta23@gmail.com>
2020-02-19 15:18:43 +0100

    [grpc] block api implementation #250

--- src/api/grpc/Block.cpp
+++ src/api/grpc/Block.cpp
@@ -7,24 +7,28 @@ Block::Block(Bot *bot) : Api::Api(bot) {}
 Block::gStatus Block::by_id(gServerContext *context,
                             const messages::Hash *request,
                             messages::Block *response) {
+  auto block = Api::block(*request);
+  response->CopyFrom(block);
   return gStatus::OK;
 }
 
-Block::gStatus Block::by_height(gServerContext *context,
-                                const gUInt64 *request,
+Block::gStatus Block::by_height(gServerContext *context, const gUInt64 *request,
                                 messages::Block *response) {
+  auto block = Api::block(request->value());
+  response->CopyFrom(block);
   return gStatus::OK;
 }
 
-Block::gStatus Block::last(gServerContext *context,
-                           const gUInt64 *request,
+Block::gStatus Block::last(gServerContext *context, const gUInt64 *request,
                            messages::Block *response) {
+  auto blocks = Api::last_blocks(request->value());
+  response->CopyFrom(blocks);
   return gStatus::OK;
 }
 
-Block::gStatus Block::total(gServerContext *context,
-                            const gEmpty *request,
+Block::gStatus Block::total(gServerContext *context, const gEmpty *request,
                             messages::_NCCAmount *response) {
+  response->set_value(Api::total_nb_blocks());
   return gStatus::OK;
 }
 
