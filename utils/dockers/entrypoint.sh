689e027e972e0727069bbb86d4b26bcc710bf258
nepta <nepta23@gmail.com>
2020-03-12 11:59:02 +0100

    remove ld_preload #249

--- utils/dockers/entrypoint.sh
+++ utils/dockers/entrypoint.sh
@@ -3,4 +3,4 @@
 mkdir /tmp/db && nohup mongod --dbpath /tmp/db &
 sleep 5
 pwd;ls -l
-LD_PRELOAD=/home/neuro/root/lib/cmake/pistache/lib/libpistache.so ./main -c conf/bot.json
+./main -c conf/bot.json
