bd20f81e81dc0eb0d8840265e7030ce0be180ce9
nepta <nepta23@gmail.com>
2020-05-19 14:49:07 +0200

    [grpc] use published block (added to the ledger) for block watcher #254

--- src/api/grpc/Block.cpp
+++ src/api/grpc/Block.cpp
@@ -3,22 +3,23 @@
 namespace neuro::api::grpc {
 
 Block::Block(Bot *bot) : Api::Api(bot) {
-  Api::subscribe(messages::Type::kBlock, [this](const messages::Header &header,
-                                                const messages::Body &body) {
-    _block_watcher.handle_new_element(body.block());
-  });
+  Api::subscribe(
+      messages::Type::kPublish,
+      [this](const messages::Header &header, const messages::Body &body) {
+        _block_watcher.handle_new_element(body.publish().block());
+      });
 }
 
 Block::Status Block::by_id(ServerContext *context,
-                            const messages::Hash *request,
-                            messages::Block *response) {
+                           const messages::Hash *request,
+                           messages::Block *response) {
   auto block = Api::block(*request);
   response->Swap(&block);
   return Status::OK;
 }
 
 Block::Status Block::by_height(ServerContext *context, const UInt64 *request,
-                                messages::Block *response) {
+                               messages::Block *response) {
   auto block = Api::block(request->value());
   response->Swap(&block);
   return Status::OK;
@@ -32,16 +33,17 @@ Block::Status Block::lasts(ServerContext *context, const UInt64 *request,
 }
 
 Block::Status Block::total(ServerContext *context, const Empty *request,
-                            messages::_NCCAmount *response) {
+                           messages::_NCCAmount *response) {
   response->set_value(Api::total_nb_blocks());
   return Status::OK;
 }
 
 Block::Status Block::watch(ServerContext *context, const Empty *request,
-                                BlockWriter *writer) {
-  _block_watcher.watch([&writer](const std::optional<messages::Block>& last_block) {
-    return writer->Write(last_block.value());
-  });
+                           BlockWriter *writer) {
+  _block_watcher.watch(
+      [&writer](const std::optional<messages::Block> &last_block) {
+        return writer->Write(last_block.value());
+      });
   return Status::OK;
 }
 
